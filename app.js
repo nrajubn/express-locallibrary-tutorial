60ec177c203c22c681a1819418de4473ec72f3bc
boddu nooka raju <bnrajums@gmail.com>
2020-04-10 16:15:19 -0500

    app.js

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
 
-var app = express();
+let app = express();
 
-var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://<rajubn>:<Pubg12>@cluster0-kwchd.azure.mongodb.net/local_library?retryWrites=true&w=majority';
+let mongoose = require('mongoose');
+const mongoDB = 'mongodb+srv://<rajubn>:<Pubg12>@cluster0-kwchd.azure.mongodb.net/local_library?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, { useNewUrlParser: true,useUnifiedTopology:true });
-var db = mongoose.connection;
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
