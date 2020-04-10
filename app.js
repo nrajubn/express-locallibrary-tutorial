ed98a8135e5b070db94635ff97d3e716cb72a541
boddu nooka raju <bnrajums@gmail.com>
2020-04-10 15:57:25 -0500

    app js commited

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb+srv://<rajubn>:<Pubg12>@cluster0-kwchd.azure.mongodb.net/local_library?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, { useNewUrlParser: true,useUnifiedTopology:true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
